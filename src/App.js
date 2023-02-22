c7be17e9dfa8506a29faf81efd89878298b234b6
MateuszKopczynski <mateusz.kopczynski23@gmail.com>
2023-02-22 12:35:24 +0100

    Add not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Index from './pages/Index';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Create from './pages/Create';
 import Show from './pages/Show';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
               <Route path="/" element={<Index />} />
               <Route path="/create" element={<Create />} />
               <Route path="/recipes/:recipeId" element={<Show />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </div>
